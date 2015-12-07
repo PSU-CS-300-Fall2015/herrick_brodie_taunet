e4f98720c6ac3c2eb40a3a4ec3108a284c7ab77f
brodie1 <brodie.herrick@gmail.com>
2015-12-06 21:46:28 -0800

    broke client into more functions

--- client.py
+++ client.py
@@ -7,14 +7,15 @@ import select
 import socket
 import sys
 
+PORT = 6283
+
 
 def client():
     class Node(object):
         # structure of server information
         def __init__(self):
-            self.hostname = None
+            self.name = None
             self.host = None
-            self.port = None
             self.next = None
 
     class List(object):
@@ -22,29 +23,29 @@ def client():
         def __init__(self):
             self.head = None
 
-        def insert(self):
+        def insert(self, name, host):
+            print('test')
+            # init new node
             new_node = Node()
 
-            # get node data
-            sys.stdout.write('please enter the hosts display name: ')
-            new_node.hostname = sys.stdin.readline()
-            sys.stdout.write('please enter the hosts address: ')
-            new_node.host = sys.stdin.readline()
-            sys.stdout.write('please enter the hosts port: ')
-            new_node.port = sys.stdin.readline()
+            # set node data
+            new_node.name = name
+            new_node.host = host
             new_node.next = None
 
+            # insert node into list
             new_node.next = self.head
             self.head = new_node
 
         def display_all(self):
             current = self.head
             while current:
-                print('Hostname:', current.hostname, '\nHost: ',
-                      current.host, '\nPort:', current.port)
+                print('name:', current.name, '\nhost: ', current.host)
                 current = current.next
 
         def delete_node(self, name):
+            # search for and remove node, if found
+            # otherwise, display error
             current = self.head
             prev = None
             found = False
@@ -62,6 +63,8 @@ def client():
                 prev.next = current.next
 
         def get_node(self, name):
+            # search for node and return it
+            # otherwise, display error
             current = self.head
             found = False
             while current and found is False:
@@ -74,81 +77,87 @@ def client():
             else:
                 return current
 
-    # set connection type
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Set socket timeout
-    s.settimeout(2)
-
     # init data
     slist = List()
-    host = ''
-    port = 0
 
     print('welcome to taunet')
+    menu(slist)
 
-    while 1:
-        print('please select an option:\n'
-              '1) add taunet node\n'
-              '2) send message\n'
-              '3) delete node\n'
-              '4) display all nodes\n'
-              '5) quit ')
-        choice = sys.stdin.readline()
-        if int(choice) == 1:
-            slist.insert()
-        elif int(choice) == 2:
-            sys.stdout.write('which host would you like to send a message to? ')
-            hostname = sys.stdin.readline()
-            node = slist.get_node(hostname)
-            host = node.host
-            port = node.port
-        elif int(choice) == 3:
-            sys.stdout.write('which host would you like to delete (please enter hostname)? ')
-            hostname = sys.stdin.readline()
-            slist.delete_node(hostname)
-        elif int(choice) == 4:
-            slist.display_all()
-        elif int(choice) == 5:
-            sys.exit()
-        else:
-            print('input not understood, try again')
+
+def menu(slist):
+    print('please select an option:\n'
+          '1) add taunet node\n'
+          '2) send message\n'
+          '3) delete node\n'
+          '4) display all nodes\n'
+          '5) quit ')
+    choice = sys.stdin.readline()
+    if int(choice) == 1:
+        sys.stdout.write('please enter the hosts display name: ')
+        name = sys.stdin.readline()
+        sys.stdout.write('please enter the hosts address: ')
+        host = sys.stdin.readline()
+        slist.insert(name, host)
+    elif int(choice) == 2:
+        sys.stdout.write('which host would you like to send a message to? ')
+        hostname = sys.stdin.readline()
+        node = slist.get_node(hostname)
+        host = node.host
+        send_msg(host, slist)
+    elif int(choice) == 3:
+        sys.stdout.write('which host would you like to delete (please enter hostname)? ')
+        hostname = sys.stdin.readline()
+        slist.delete_node(hostname)
+    elif int(choice) == 4:
+        slist.display_all()
+    elif int(choice) == 5:
+        sys.exit()
+    else:
+        print('input not understood, try again')
+
+
+def send_msg(host, slist):
+    # set connection type
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # set socket timeout
+    srv.settimeout(2)
 
     # connect to host
     try:
-        s.connect((host, port))
+        srv.connect(host, PORT)
     except:
-        print('Unable to connect')
+        print('unable to connect')
 
-    print("Connected to remote host. You can start sending messages")
+    print("connected to remote host. you can start sending messages")
     sys.stdout.write('[Me] '); sys.stdout.flush()
 
     while 1:
-        socket_list = [sys.stdin, s]
+        socket_list = [sys.stdin, srv]
 
         # Get the list of readable sockets
         ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [], 0)
 
         for sock in ready_to_read:
-            if sock == s:
+            if sock == srv:
                 # Message from server
                 data = sock.recv(4096)
-                print('reading from server')
                 if not data:
-                    print('\nDisconnected from chat server')
+                    print('\ndisconnected from chat server')
                     sys.exit()
                 else:
-                    print('got data from server')
                     sys.stdout.write(data)
                     sys.stdout.write('[Me] ')
 
             else:
                 # User has entered a message
+                print('when finished, please type /quit')
                 msg = sys.stdin.readline()
-                print('message entered: {}'.format(msg))
-                s.send(msg.encode('utf-8'))
-                print('message sent')
+                if msg is '/quit':
+                    break
+                srv.send(msg.encode('utf-8'))
                 sys.stdout.write('[Me] ')
+        menu(slist)
 
 if __name__ == "__main__":
     sys.exit(client())
